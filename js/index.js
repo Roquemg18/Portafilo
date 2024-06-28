e77d2ff37ead23d99833318b7f7fdc2f0ef1183f
roquemg <molinaroque871@gmail.com>
2024-06-28 15:25:14 -0300

    nueva forma de mostrar los projectos

--- js/index.js
+++ js/index.js
@@ -7,8 +7,6 @@ btnSwitch.addEventListener("click", () => {
 });
 
 // create cards project
-const itemContainerId = document.querySelector("#itemContainerId");
-
 async function getProjects() {
   try {
     //busca los datos de los projectos y los asigna a una variable
@@ -22,30 +20,54 @@ async function getProjects() {
 
 async function render() {
   const projects = await getProjects();
-  renderProjects(projects);
+  generateProductHTML(projects);
+  configSwiper();
 }
 
-function renderProjects(projects) {
+// Contenedor donde se insertarán los productos
+const container = document.getElementById("project-container");
+
+// Función para generar el HTML de cada producto
+function generateProductHTML(projects) {
   projects.forEach((project) => {
     const newProject = document.createElement("div");
-    newProject.className = "project";
+    newProject.className = "swiper-slide";
     newProject.setAttribute("data-id", project.id);
-    newProject.innerHTML =
-      //estructura de la card que muestra el projecto
-      `
-      <div class="project-img">
-        <img src="${project.img}" alt="${project.title}">
-        <h1>${project.title}</h1>
-      </div>
-      <div class="project-buttons">
-        <a href="${project.urlGitHub}" target="_blank">GitHub</a>
-        <a href="${project.urlServer}" target="_blank">Live Demo </a>
-      </div>`;
-    itemContainerId.append(newProject);
+    newProject.innerHTML = `
+            <div class="project-content">
+                <div class="project-txt">
+                    <h3>${project.title}</h3>
+                    <p>${project.description}</p>
+                </div>
+                <div class="project-img">
+                    <img class="projectImg" src="${project.img}" />
+                </div>
+            </div>
+            <div class="containerButtonProject">
+                <a href="${project.urlGitHub}" class="btnProject">gitHub</a>
+                <a href="${project.urlServer}" class="btnProject">server</a>
+            </div>
+    `;
+    container.append(newProject);
+  });
+}
+
+function configSwiper() {
+  // Inicializar Swiper
+  var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    coverflowEffect: {
+      depth: 500,
+      modifier: 1,
+      slideShadows: true,
+      rotate: 0,
+      stretch: 0,
+    },
   });
 }
 
 render();
-
-renderProjects();
-
